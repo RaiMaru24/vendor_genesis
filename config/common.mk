fffce92d7c842f431b63ab54067d342bdb038c9e
Jesse Chan <jc@lineageos.org>
2020-05-19 14:01:25 +0800

    Implement hide gestural navigation hint bar [3/3]
    Change-Id: I5ed5c55ec46c3bb5e91aeb88bd5f06bd4445c2f0
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- config/common.mk
+++ config/common.mk
@@ -212,6 +212,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     CustomPixelLauncherOverlay \
     DocumentsUIOverlay \
+    NavigationBarNoHintOverlay \
     NetworkStackOverlay
 
 # Translations
